20dd3bacb176170c855c4ed89bea5d38df618c58
sergey radionov <rsatom@gmail.com>
2018-11-03 19:59:52 +0700

    make only "/test" path public
    for authenticationRequired callback example

--- RestreamServerApp/main.cpp
+++ RestreamServerApp/main.cpp
@@ -6,13 +6,30 @@ extern "C" {
 GST_PLUGIN_STATIC_DECLARE(interpipe);
 }
 
+bool authenticationRequired(GstRTSPMethod method, const std::string& path)
+{
+    const std::string publicPath = "/test";
+
+    // on RTSP SETUP path could contain control id of the stream
+    if(path == publicPath ||
+        (method == GST_RTSP_SETUP &&
+         0 == path.compare(0, publicPath.length(), publicPath) &&
+         0 == path.compare(publicPath.length(), 1, "/")))
+    {
+         return false;
+    }
+
+    return true;
+}
+
 int main(int argc, char *argv[])
 {
     gst_init(0, nullptr);
 
     GST_PLUGIN_STATIC_REGISTER(interpipe);
 
-    RestreamServer::Server restreamServer({});
+    RestreamServer::Server restreamServer(
+        { .authenticationRequired = authenticationRequired });
 
     restreamServer.serverMain();
 
