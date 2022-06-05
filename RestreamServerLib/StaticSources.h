3b15fc47a78ca8a4581ee0cf0396abb4500c1f4f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-05 14:03:11 +0800

    Delete RestreamServerLib directory

--- RestreamServerLib/StaticSources.h
+++
@@ -1,8 +0,0 @@
-
-#define BARS  "/bars"
-#define NOISE "/noise"
-#define WHITE "/white"
-#define BLACK "/black"
-#define RED   "/red"
-#define GREEN "/green"
-#define BLUE  "/blue"
--- RestreamServerLib/Types.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-#include <string>
-
-typedef std::string Host;
-typedef std::string URL;
-typedef uint16_t Port;
